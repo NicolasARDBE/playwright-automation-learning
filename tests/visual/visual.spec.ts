0d6a8efc83e28c246e21a1c238c444b8fbbe56cf
NicolasARDBE <na.reyesd@javeriana.edu.co>
2025-09-24 19:15:29 -0500

    fix: accepting 10% difference ration in images

--- tests/visual/visual.spec.ts
+++ tests/visual/visual.spec.ts
@@ -3,13 +3,17 @@ import { test, expect } from '@playwright/test';
 test.describe('Visual Regression Tests', () => {
     test('Full page snapshot', async ({ page }) => {
         await page.goto('https://www.example.com');
-        expect(await page.screenshot()).toMatchSnapshot('example-com.png');
+        expect(await page.screenshot()).toMatchSnapshot('example-com.png', {
+            maxDiffPixelRatio: 0.1,
+        });
     });
 
     test.only('Element snapshot', async ({ page }) => {
         await page.goto('https://www.example.com');
         const element = await page.$('h1'); //En este caso no se espera a que el elemento exista para la continuidad del test. Puede ser null
-        expect(await element?.screenshot()).toMatchSnapshot('example-com-h1.png');
+        expect(await element?.screenshot()).toMatchSnapshot('example-com-h1.png', {
+            maxDiffPixelRatio: 0.1,
+        });
     });
 });
 
